8dbfa2e83dff2b3caf116fa03d5dbc371d0f24dd
Janet35 <janetl35@nycstudents.net>
2025-04-10 10:17:24 -0400

    commit

---
+++ flashcards.py
@@ -0,0 +1,9 @@
+class Janet:
+    def display_info(self):
+        return f"Teacher: {self.trait}, Email: {self.phrase}, Subject: {self.song}"
+
+    def __init__(trait, phrase, song):
+        super().__init__(song, phrase)
+        self.trait = trait
+    
+janet = ('amazing', 'im so fire yo', 'only girl')
