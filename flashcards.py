1c23ab5640b4edba6a7974a67f93c555d9b049b7
Janet35 <janetl35@nycstudents.net>
2025-04-22 10:16:18 -0400

    commit

--- flashcards.py
+++ flashcards.py
@@ -6,13 +6,23 @@ class flashcard:
         self.answer = answer
     
     def display_info(self):
-        return f"{self.question} {self.answer}"
+        return f"{self.question}"
     
     def to_dict(self):
         return {"answer": self.question, "answer": self.answer}
-    
+
 flashcards = [
-    flashcard('Who is ur goat?', 'evb is my goat'),
-    flashcard('Name three models: ', 'alex consani, niamph campbell, gigi hadid') ]
+    flashcard('Name three models: ', 'alex consani, naomi campbell, gigi hadid'),
+    flashcard("What year did The Weeknd perform at a Victoria's Secret Fashion Show in Paris?", "2016"),
+    flashcard("when did Zendaya get engaged?", '2025') 
+    ]
 
 flashcards_data = [flashcard.to_dict() for flashcard in flashcards]
+with open("flashcards.json", "w") as file:
+    json.dump(flashcards_data, file, indent=4)
+
+print(flashcard.display_info())
+ask = input
+if input == flashcard.answer:
+    streak =+ 1
+    print("good job!! Streak: ", streak)
